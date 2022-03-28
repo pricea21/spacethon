d2c1afdd00e52493c5f97d478377f6da840c9941
pricea21 <pricea26@my.erau.edu>
2022-03-27 18:12:57 -0700

    Assets

---
+++ Assets/Scripts/PlayerColllision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerColllision : MonoBehaviour
+{
+	public PlayerMovement movement;
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "obstacle")
+		{
+			movement.enabled = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+
+}
